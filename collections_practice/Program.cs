230e77ec0ad243e3703b0658ff29b9d1a9b0d6bb
oforie <oforie@uw.edu>
2017-08-02 15:25:34 -0700

    initial commit

---
+++ collections_practice/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace collections_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");  
+            int[] myarr1 = new int[10] {0,1,2,3,4,5,6,7,8,9};
+            string[] myarr2 = new string[4] {"Tim", "Martin", "Nikki", "Sarah"};
+            bool[] myarr3 = new bool[10];
+            for(int i=0; i<10; i+=2){
+                myarr3[i] = true;
+            }
+
+            int[,] multip = new int[10,10];
+            for(int x=0; x<10; x++){
+            for(int y=0; y<10; y++){
+                multip[x, y] = (x+1) * (y+1);
+            }
+        }
+        //multiplication table
+        
+        }
+    }
+       
+
+}
+
+
+
+
+    
---
+++ fundamentals_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace fundamentals_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // first_loop(1,255);
+            // second_loop(1, 100);
+            third_loop(1, 100);
+            Console.WriteLine("Hello World!");
+        }
+
+        static void first_loop(int start, int end){
+            for (int i=start; i<=end; i++){
+                Console.WriteLine(i);
+            }
+
+        }
+
+        static void second_loop(int start, int end){
+            for (int i=start; i<=end; i++){
+                if (i % 15 == 0){
+                    continue;
+                } else if  (i % 3 == 0 || i % 5 == 0) {
+                Console.WriteLine(i);
+                }
+            }
+
+        }
+
+     static void third_loop(int start, int end){
+            for (int i=start; i<=end; i++){
+                if (i % 15 == 0){
+                    Console.WriteLine("FizzBuzz");
+                } else if  (i % 3 == 0){
+                    Console.WriteLine("Fizz");
+                } else if (i % 5 == 0){
+                    Console.WriteLine("Buzz");
+                }
+            }
+
+        }
+    }
+}
